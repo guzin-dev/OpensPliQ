d016fb8b1f16ba1c34866d2f8b26029c32369087
Gustavo Braga <guzinhojk99@gmail.com>
2024-09-12 18:36:08 -0300

    asdasdas

--- script.js
+++ script.js
@@ -1,49 +1,48 @@
-const requestOptions = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    pliq_token: '0e684b95-77a7-400f-a3c4-f668a9d6a2a3', // Adicionando o pliq_token no cabeçalho
-  },
-  body: JSON.stringify({
-    nameImport: 'Teste',
-    update_import: '1',
-    scheduling: false,
-    date_scheduling: '',
-    customers: [
-      {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        phone: '',
-        tags: '{"Tags":""}',
-        fk_type_sexo: null,
-        number: null,
-        properties: [
-          {
-            id_property: 353,
-          },
-        ],
-        properties_contacts: null,
-      },
-    ],
-    baseLegal: '2',
-    flg_email: false,
-    flg_popup: true,
-    flg_whatsapp: false,
-    flg_sms: false,
-    flg_manual: true,
-    fk_journey: null,
-    abstract_validators: false,
-    url_survey: 'f499d34bd87b42948b3960b8f6b82e74',
-  }),
-};
-
-// URL da requisição
-const url = 'https://sandbox-api.pliq.io/v2/api/import';
-
 // Executando a requisição
 function createAccount() {
   if (!document.getElementById('name').value) return alert('Insira um nome');
   if (!document.getElementById('email').value) return alert('Insira um email');
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      pliq_token: '0e684b95-77a7-400f-a3c4-f668a9d6a2a3', // Adicionando o pliq_token no cabeçalho
+    },
+    body: JSON.stringify({
+      nameImport: 'Teste',
+      update_import: '1',
+      scheduling: false,
+      date_scheduling: '',
+      customers: [
+        {
+          name: document.getElementById('name').value,
+          email: document.getElementById('email').value,
+          phone: '',
+          tags: '{"Tags":""}',
+          fk_type_sexo: null,
+          number: null,
+          properties: [
+            {
+              id_property: 353,
+            },
+          ],
+          properties_contacts: null,
+        },
+      ],
+      baseLegal: '2',
+      flg_email: false,
+      flg_popup: true,
+      flg_whatsapp: false,
+      flg_sms: false,
+      flg_manual: true,
+      fk_journey: null,
+      abstract_validators: false,
+      url_survey: 'f499d34bd87b42948b3960b8f6b82e74',
+    }),
+  };
+
+  // URL da requisição
+  const url = 'https://sandbox-api.pliq.io/v2/api/import';
   fetch(url, requestOptions)
     .then((response) => {
       console.log(response);
